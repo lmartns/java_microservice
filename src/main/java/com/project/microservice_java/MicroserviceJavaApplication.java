9a2627642b99abf4054beee4713a75be0e0040d3
lmartns <leonardomartins578@gmail.com>
2024-09-15 21:12:47 -0300

    ✨ feat: add initial project structure with configuration files and base code

---
+++ src/main/java/com/project/microservice_java/MicroserviceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.project.microservice_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceJavaApplication.class, args);
+	}
+
+}
