dddd4efa37d353119a7b026dbd97f84d3122147e
lmartns <leonardomartins578@gmail.com>
2024-09-19 21:52:00 -0300

    📇 Initial database configuration

--- src/main/java/com/project/microservice_java/MicroserviceJavaApplication.java
+++ src/main/java/com/project/microservice_java/MicroserviceJavaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicroserviceJavaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceJavaApplication.class, args);
 	}
-
 }
