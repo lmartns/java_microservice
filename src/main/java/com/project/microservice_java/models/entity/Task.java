bb746735de2e81b8a4cbd863da744e8f48a57900
lmartns <leonardomartins578@gmail.com>
2024-09-22 14:27:08 -0300

    ✨ feat: add Task entity and update pom.xml

---
+++ src/main/java/com/project/microservice_java/models/entity/Task.java
@@ -0,0 +1,23 @@
+package com.project.microservice_java.models.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column
+    @NotNull
+    private String name;
+
+    @Column
+    @NotNull
+    private String description;
+}
